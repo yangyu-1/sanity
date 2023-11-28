e739e530e7139514766e22d6b103aaaa9b7818e1
Robin Pyon <robin.pyon@gmail.com>
2023-11-28 16:45:54 +0000

    feat: add custom scrollbar styles to non-webkit browsers

--- packages/sanity/src/core/studio/GlobalStyle.tsx
+++ packages/sanity/src/core/studio/GlobalStyle.tsx
@@ -1,4 +1,4 @@
-import {createGlobalStyle} from 'styled-components'
+import {createGlobalStyle, css} from 'styled-components'
 
 const SCROLLBAR_SIZE = 14 // px
 const SCROLLBAR_BORDER_SIZE = 4 // px
@@ -6,41 +6,41 @@ const SCROLLBAR_BORDER_SIZE = 4 // px
 export const GlobalStyle = createGlobalStyle(({theme}) => {
   const {color, fonts} = theme.sanity
 
-  return {
-    '::-webkit-scrollbar': {
-      width: `${SCROLLBAR_SIZE}px`,
-      height: `${SCROLLBAR_SIZE}px`,
-    },
+  return css`
+    ::-webkit-scrollbar {
+      width: ${SCROLLBAR_SIZE}px;
+      height: ${SCROLLBAR_SIZE}px;
+    }
 
-    '::-webkit-scrollbar-thumb': {
-      backgroundClip: 'content-box',
-      backgroundColor: color.muted.transparent.disabled.fg,
-      borderRadius: `${SCROLLBAR_SIZE}px`,
-      border: `${SCROLLBAR_BORDER_SIZE}px solid transparent`,
-    },
+    ::-webkit-scrollbar-thumb {
+      background-clip: content-box;
+      background-color: ${color.muted.transparent.disabled.fg};
+      border-radius: ${SCROLLBAR_SIZE}px;
+      border: ${SCROLLBAR_BORDER_SIZE}px solid transparent;
+    }
 
-    '::-webkit-scrollbar-thumb:hover': {
-      backgroundColor: color.muted.transparent.enabled.fg,
-    },
+    ::-webkit-scrollbar-thumb:hover {
+      background-color: ${color.muted.transparent.enabled.fg};
+    }
 
-    '::-webkit-scrollbar-track': {
-      background: 'transparent',
-    },
+    ::-webkit-scrollbar-track {
+      background: transparent;
+    }
 
-    html: {
-      backgroundColor: color.base.bg,
-    },
+    html {
+      background-color: ${color.base.bg};
+    }
 
-    '#sanity': {
-      fontFamily: fonts.text.family,
-    },
+    #sanity {
+      font-family: ${fonts.text.family};
+    }
 
-    b: {
-      fontWeight: fonts.text.weights.medium,
-    },
+    b {
+      font-weight: ${fonts.text.weights.medium};
+    }
 
-    strong: {
-      fontWeight: fonts.text.weights.medium,
-    },
-  }
+    strong {
+      font-weight: ${fonts.text.weights.medium};
+    }
+  `
 })
